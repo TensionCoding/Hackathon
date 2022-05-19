cf7e6d4bf87fa7e082fc65d196a3319cc4831dfa
TensionCoding <wdd4services@outlook.com>
2022-05-18 21:25:20 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const contents = document.getElementById('contents');
+contents.parentNode.removeChild(contents);
+
+
+/*
+let lastCalled = 0;
+const now = Date.now()
+if(now - lastCalled >= wait){
+    lastCalled = now;
+    return func(...args)
+}
+*/
+
+// Frontend 
+// get it to display in the top right of the window
+// div to display image 
+
+// Back End 
+// blacklist websites - eg manifest matching website
+    // youtube
+    // instagram 
+    // tiktok
+  // creature shows up 
+  // timer goes off 
+  // at set invervals, creature shrivels 
+    // using date now method 
+    // removing current image
+    // appending array of deteriorating images 
+// when it does match
+// function to 
+
+
+// stretch features
+// can customize blacklist websites
+// animal gets better at normal sites 
+
